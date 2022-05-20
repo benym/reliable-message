052dd7a3b42c755ce0150ca2ccda6f0661d62274
benym <38175888+benym@users.noreply.github.com>
2022-05-20 16:17:22 +0800

    更新kafka配置，支持手动ack

--- src/main/java/com/test/message/config/KafkaConfig.java
+++ src/main/java/com/test/message/config/KafkaConfig.java
@@ -1,11 +1,15 @@
 package com.test.message.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConsumerRecordRecoverer;
+import org.springframework.kafka.listener.ContainerProperties.AckMode;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.ErrorHandler;
 import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
@@ -16,17 +20,33 @@ import org.springframework.util.backoff.FixedBackOff;
 @Configuration
 public class KafkaConfig {
 
+
+    @Autowired
+    private ConsumerFactory consumerFactory;
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory containerFactory() {
+        ConcurrentKafkaListenerContainerFactory factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(consumerFactory);
+        // 如果consumer有ack入参，则这句话是必须的
+        factory.getContainerProperties().setAckMode(AckMode.MANUAL);
+        // 最大重试次数3次
+        factory.setErrorHandler(kafkaErrorHandler(kafkaTemplate));
+        return factory;
+    }
+
     /**
      * 消费端重试机制，超过重试次数则加入到死信队列
      *
      * @param template template
      * @return ErrorHandler
      */
-    @Bean
-    @Primary
     public ErrorHandler kafkaErrorHandler(KafkaTemplate<?, ?> template) {
         log.warn("kafkaErrorHandler begin to Handle");
-        // 创建DLT对象，死信队列命名规则：原有Topic+.DTL后缀=死信队列Topic
+        // 创建DLT对象，死信队列命名规则：原有Topic+.DLT后缀=死信队列Topic
         ConsumerRecordRecoverer recoverer = new DeadLetterPublishingRecoverer(template);
         // 设置重试间隔为10秒，次数3次
         BackOff backOff = new FixedBackOff(10 * 1000L, 3);
