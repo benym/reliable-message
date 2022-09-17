bf10bf95f7be2423ba5c22b364fc876ed75fe3b4
benym <benyuanming@gmail.com>
2022-09-17 14:29:36 +0800

    [feature]新增long支持

--- src/main/java/com/test/message/config/JacksonConfig.java
+++ src/main/java/com/test/message/config/JacksonConfig.java
@@ -1,29 +1,23 @@
 package com.test.message.config;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.TimeZone;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * Jackson配置
@@ -68,7 +62,9 @@ public class JacksonConfig {
                                     DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)))
                     .deserializerByType(LocalTime.class,
                             new LocalTimeDeserializer(
-                                    DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
+                                    DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)))
+                    .serializerByType(Long.class, ToStringSerializer.instance)
+                    .serializerByType(Long.TYPE, ToStringSerializer.instance);
         };
     }
 }
